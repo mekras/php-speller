b323033d09c4fb12659b7a774650eee29492fc28
Михаил Красильников <m.krasilnikov@yandex.ru>
2016-04-19 17:27:04 +0300

    Update tests config.

--- tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-$loader = require_once __DIR__ . '/../vendor/autoload.php';
-$loader->addPsr4('Mekras\\Speller\\Tests\\', __DIR__);
-
-@date_default_timezone_set(date_default_timezone_get());
