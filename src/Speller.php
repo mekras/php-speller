06fd8a594c424a3154635f90e0868d09d87f6efe
Михаил Красильников <m.krasilnikov@yandex.ru>
2017-03-11 11:56:09 +0300

    Formatting

--- src/Speller.php
+++ src/Speller.php
@@ -6,24 +6,25 @@
  * @author    Михаил Красильников <m.krasilnikov@yandex.ru>
  * @license   http://opensource.org/licenses/MIT MIT
  */
+
 namespace Mekras\Speller;
 
 use Mekras\Speller\Source\Source;
 
 /**
- * Speller interface
+ * Speller interface.
  *
  * @since 1.0
  */
 interface Speller
 {
     /**
-     * Check text
+     * Check text.
      *
      * Check given text and return an array of spelling issues.
      *
-     * @param Source $source    text source to check
-     * @param array  $languages list of languages used in text (IETF language tag)
+     * @param Source $source    Text source to check.
+     * @param array  $languages List of languages used in text (IETF language tag).
      *
      * @return Issue[]
      *
@@ -33,7 +34,7 @@ interface Speller
     public function checkText(Source $source, array $languages);
 
     /**
-     * Return list of supported languages
+     * Return list of supported languages.
      *
      * @return string[]
      *
