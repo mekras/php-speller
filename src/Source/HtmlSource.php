66ae2774c0c92f7bf2c2aa8054cb53f2fb2bff92
Михаил Красильников <m.krasilnikov@yandex.ru>
2017-03-12 15:32:17 +0300

    HtmlSource should be derived from MetaSource

--- src/Source/HtmlSource.php
+++ src/Source/HtmlSource.php
@@ -14,23 +14,73 @@ use Mekras\Speller\Source\Filter\HtmlFilter;
 /**
  * HTML document as a text source.
  *
+ * @since x.x derived from MetaSource.
  * @since 1.6 derived from StringSource.
  * @since 1.5
  */
-class HtmlSource extends StringSource
+class HtmlSource extends MetaSource
 {
+    /**
+     * Source text.
+     *
+     * @var string
+     */
+    private $text;
+
+    /**
+     * Text encoding.
+     *
+     * @var string
+     */
+    private $encoding;
+
     /**
      * Create text source from HTML.
      *
-     * @param string $html
+     * @param EncodingAwareSource|string $source
      *
+     * @throws \Mekras\Speller\Exception\SourceException
+     *
+     * @since x.x Accepts EncodingAwareSource
      * @since 1.5
+     *
+     * @todo  deprecate string $source in version 2.0
      */
-    public function __construct($html)
+    public function __construct($source)
     {
+        if (!$source instanceof EncodingAwareSource) {
+            $source = new StringSource($source);
+        }
+        parent::__construct($source);
+        $html = $this->source->getAsString();
         $document = new \DOMDocument('1.0');
         $document->loadHTML($html);
+        $this->encoding = $document->encoding;
         $filter = new HtmlFilter();
-        parent::__construct($filter->filter($html), $document->encoding);
+        $this->text = $filter->filter($html);
+    }
+
+    /**
+     * Return text as one string
+     *
+     * @return string
+     *
+     * @since 1.0
+     */
+    public function getAsString()
+    {
+        return (string) $this->text;
+    }
+
+    /**
+     * Return source text encoding.
+     *
+     * @return string
+     *
+     * @since x.x
+     */
+    public function getEncoding()
+    {
+        return $this->encoding;
     }
 }
